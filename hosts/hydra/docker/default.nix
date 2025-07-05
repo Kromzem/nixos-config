539ed5d3904b407cd4ca845391ea2dfef71ad711
palikin <palikin@tutamail.com>
2025-07-05 10:19:25 +0200

    added jelly app

--- hosts/hydra/docker/default.nix
+++ hosts/hydra/docker/default.nix
@@ -5,6 +5,7 @@
     ./satisfactory.nix
     ./stirling_pdf.nix
     ./jellyfin.nix
+    ./jelly-app.nix
     ./planar_ally.nix
     # ./it-tools.nix # does not start as of 12.06.2025
     # ./jellyfin.nix
